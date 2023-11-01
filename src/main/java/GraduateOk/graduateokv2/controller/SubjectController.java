b79bb900e75239ae64ec1bdc7df6a4509c6935b4
clapbin0320 <83696912+clapbin0320@users.noreply.github.com>
2023-11-01 15:14:19 +0900

    refactor: 인기교양추천 목록 조회 API 정렬 수정
    - 정렬 교양 수강횟수 내림차순에 교양 과목명 오름차순 조건 추가

--- src/main/java/GraduateOk/graduateokv2/controller/SubjectController.java
+++ src/main/java/GraduateOk/graduateokv2/controller/SubjectController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,11 @@ public class SubjectController {
     public BaseResponse<SubjectResponse.Rank> getSubjectRank(@RequestParam(required = false) String searchWord,
                                                              @RequestParam(required = false) String type,
                                                              @RequestParam(required = false) Integer credit,
-                                                             @PageableDefault(size = 30, sort = "kyCount", direction = Sort.Direction.DESC)
+                                                             @PageableDefault(size = 30)
+                                                             @SortDefault.SortDefaults({
+                                                                     @SortDefault(sort = "kyCount", direction = Sort.Direction.DESC),
+                                                                     @SortDefault(sort = "name", direction = Sort.Direction.ASC)
+                                                             })
                                                              Pageable pageable) {
 
         return BaseResponse.ok(HttpStatus.OK, subjectService.getSubjectRank(searchWord, type, credit, pageable));
