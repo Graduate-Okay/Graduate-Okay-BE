ead4fa9f1e99b265c2d4132a86d9e8538d7eb7f3
clapbin0320 <83696912+clapbin0320@users.noreply.github.com>
2023-11-08 16:35:28 +0900

    add appspec.yml, deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROJECT_ROOT="/home/ec2-user/graduateokv2"
+JAR_FILE="$PROJECT_ROOT/graduate-ok-v2-0.0.1-SNAPSHOT.jar"
+
+TIME_NOW=$(date +%c)
+
+APP_LOG="$PROJECT_ROOT/log_$TIME_NOW.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2>&1 &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
