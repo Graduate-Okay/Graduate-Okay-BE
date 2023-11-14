539830923cb83188aa135c66535283635bba0b7e
clapbin0320 <83696912+clapbin0320@users.noreply.github.com>
2023-11-14 11:32:01 +0900

    feat: PDF 관련 에러 추가

--- src/main/java/GraduateOk/graduateokv2/exception/Error.java
+++ src/main/java/GraduateOk/graduateokv2/exception/Error.java
@@ -13,6 +13,7 @@ public enum Error {
     BAD_PASSWORD(HttpStatus.BAD_REQUEST, "400_PW", "비밀번호는 8자 이상이어야 합니다."),
     BAD_EMAIL(HttpStatus.BAD_REQUEST, "400_EMAIL", "한신대학교 이메일이어야 합니다."),
     BAD_AUTH_NUMBER(HttpStatus.BAD_REQUEST, "400_AUTH", "잘못된 인증번호입니다."),
+    BAD_PDF(HttpStatus.BAD_REQUEST, "400_PDF", "한신대학교 학업성적확인서 PDF여야 합니다."),
 
     // 401 UNAUTHORIZED 권한없음(인증 실패)
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "401", "권한 인증에 실패했습니다."),
@@ -31,6 +32,7 @@ public enum Error {
     NOT_FOUND_MAJOR(HttpStatus.NOT_FOUND, "404_MAJOR", "존재하지 않는 전공입니다."),
     NOT_FOUND_SUBJECT(HttpStatus.NOT_FOUND, "404_SUBJECT", "존재하지 않는 과목입니다."),
     NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "404_REVIEW", "존재하지 않는 리뷰입니다."),
+    NOT_FOUND_PDF(HttpStatus.NOT_FOUND, "404_PDF", "PDF가 존재하지 않습니다."),
 
     // 409 CONFLICT 중복된 리소스
     ALREADY_SAVED_EMAIL(HttpStatus.CONFLICT, "409_EMAIL", "이미 가입된 이메일입니다."),
