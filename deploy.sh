d525af13387f3620f7fc2f9e93edee2b3447436b
clapbin0320 <83696912+clapbin0320@users.noreply.github.com>
2023-11-14 10:55:57 +0900

    Update deploy.sh, application.yml

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ec2-user/graduateokv2"
-JAR_FILE="$PROJECT_ROOT/graduate-ok-v2-0.0.1-SNAPSHOT.jar"
-
+JAR_FILE_NAME="graduate-ok-v2.jar"
+JAR_FILE="$PROJECT_ROOT/$JAR_FILE_NAME"
 TIME_NOW=$(date +%c)
+LOG="$PROJECT_ROOT/log_$TIME_NOW.log"
 
-APP_LOG="$PROJECT_ROOT/log_$TIME_NOW.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+# redis 설치
+sudo yum update
+sudo amazon-linux-extras install -y redis6
+sudo systemctl start redis
 
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+# 시간 변경
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
-fi
+# java kill
+sudo killall java
+sleep 5
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+# java 실행
+#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cd $PROJECT_ROOT
+sudo chmod 764 $JAR_FILE_NAME
+nohup java -jar $JAR_FILE > $LOG 2>&1 &
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2>&1 &
+disown
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+exit
