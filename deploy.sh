49c2e84bf0c66f6318232cb6770c64b6e12c4e87
clapbin0320 <clapbin0320@gmail.com>
2024-01-24 18:08:03 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-PROJECT_ROOT="/home/ec2-user/graduateokv2"
-JAR_FILE_NAME="graduate-ok-v2.jar"
-JAR_FILE="$PROJECT_ROOT/$JAR_FILE_NAME"
-TIME_NOW=$(date +%c)
-FORMATTED_DATE=$(date +'%y%m%d')
-LOG="$PROJECT_ROOT/log_$FORMATTED_DATE.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
 # 현재 구동 중인 java 종료
 PID=$(sudo lsof -i TCP -P | grep java | grep LISTEN | awk '{print $2}')
 echo $PID
@@ -19,20 +11,19 @@ if [ -n "$PID" ]; then
 fi
 
 # redis 설치
-echo "$TIME_NOW > Redis 설치" >> $DEPLOY_LOG
-sudo yum update
-sudo yum install -y redis6
+#sudo yum update
+#sudo yum install -y redis6
 sudo systemctl start redis6
 
 # 시간 변경
-echo "$TIME_NOW > 시간 변경" >> $DEPLOY_LOG
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 
 # nginx 실행
+#sudo systemctl reload nginx || sudo systemctl start nginx
 sudo systemctl start nginx
 
 # java 실행
-echo "$TIME_NOW > $JAR_FILE_NAME 파일 실행" >> $DEPLOY_LOG
-sudo chmod 777 $JAR_FILE
-nohup java -jar $JAR_FILE > $LOG 2>&1 &
+sudo chmod 777 /home/ec2-user/graduateokv2/graduate-ok-v2.jar
+FORMATTED_DATE=$(date +'%y%m%d')
+sudo nohup java -jar /home/ec2-user/graduateokv2/graduate-ok-v2.jar > /home/ec2-user/log$FORMATTED_DATE.log 2>&1 &
