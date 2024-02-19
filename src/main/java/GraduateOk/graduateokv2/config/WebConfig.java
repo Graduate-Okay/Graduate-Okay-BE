7fe4174089496e1ed98b031f9c981f30dcd3c7f0
clapbin0320 <clapbin0320@gmail.com>
2024-02-20 08:41:20 +0900

    fix: 졸업가능 도메인 cors 에러 해결

--- src/main/java/GraduateOk/graduateokv2/config/WebConfig.java
+++ src/main/java/GraduateOk/graduateokv2/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
-                .allowedOrigins("https://graduate-okay.netlify.app/", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedOrigins("https://graduate-okay.netlify.app/", "http://localhost:3000", "https://hs-graduate-ok.site/")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                 .maxAge(3000);
     }
 }
